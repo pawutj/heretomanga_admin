a7bdb07e7b71a5e49457e96e7ce4025d9b14d379
22p26c0377 <pawut.j@gmail.com>
2021-03-10 03:19:31 +0800

    fix bug update State

--- src/App.js
+++ src/App.js
@@ -4,37 +4,56 @@ import "./App.css";
 import { Selection, SelectionContainer } from "@madappgang/react-selections";
 import testImg from "./test.png";
 
-const selectionArea = {
-  id: 1,
-  dimensions: {
-    height: 150,
-    width: 300,
-  },
-  coordinates: {
-    x: 100,
-    y: 100,
-  },
-};
-
 function App() {
-  const [selectionArea, setSelectionArea] = useState({
-    id: 1,
-    dimensions: {
-      height: 150,
-      width: 300,
+  const [count, setCount] = useState([]);
+  const [selectionArea, setSelectionArea] = useState([
+    {
+      id: 1,
+
+      dimensions: {
+        height: 150,
+        width: 300,
+      },
+      coordinates: {
+        x: 100,
+        y: 100,
+      },
     },
-    coordinates: {
-      x: 100,
-      y: 100,
+    {
+      id: 2,
+
+      dimensions: {
+        height: 300,
+        width: 300,
+      },
+      coordinates: {
+        x: 100,
+        y: 100,
+      },
     },
-  });
+    {
+      id: 3,
+
+      dimensions: {
+        height: 100,
+        width: 300,
+      },
+      coordinates: {
+        x: 2,
+        y: 2,
+      },
+    },
+  ]);
 
   const createSelectionArea = () => {
-    //setSelectionArea()
+    // const ind = selectionArea.filter((c) => c.display == false)[0];
+    // const newArea = { ...ind, display: true };
+    // setSelectionArea([...selectionArea.filter((c) => c.id != ind.id), newArea]);
   };
 
   const handleSelectionAreaUpdate = (t) => {
-    console.log(t);
+    setSelectionArea([...selectionArea.filter((c) => c.id != t.id), t]);
+    console.log(selectionArea);
   };
 
   return (
@@ -48,16 +67,19 @@ function App() {
         }}
       >
         <SelectionContainer>
-          <Selection
-            backgroundImage
-            interactive
-            area={selectionArea}
-            onAreaUpdate={handleSelectionAreaUpdate}
-          />
+          {selectionArea.map((c, k) => (
+            <Selection
+              backgroundImage
+              interactive
+              area={c}
+              onAreaUpdate={handleSelectionAreaUpdate}
+            />
+          ))}
         </SelectionContainer>
       </div>
       <header className="App-header">
-        <button> Create </button>
+        <h1>{`${count.length}, ${selectionArea.length}`}</h1>
+        <button onClick={createSelectionArea}> Create </button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
